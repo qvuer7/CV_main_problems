1214bd631ba7639f261fd2cd4131a709066c9dd0
Andrii Zelenko <qvuer7@gmail.com>
2022-10-13 17:22:58 +0100

    -google colab notebook for deeplabv3 training created -config for google colab created(colab_config_dl3.py) -deeplab trained on colab for one epoch -todo modified

--- main.py
+++ main.py
@@ -1,92 +1,42 @@
 from utils.data_loading import get_data_loaders
-from utils.trainers import train_one_epoch
-from utils.models import get_deep_lab_v3
-import torch
-from config import *
 
 
-train_loader, val_loader = get_data_loaders()
-print('train loaders created')
-model = get_deep_lab_v3()
-print('model created')
-criterion = torch.nn.MSELoss(reduction = 'mean')
-optimizer = torch.optim.Adam(model.parameters(), lr = LR)
-train_loss = train_one_epoch(model = model, criterion = criterion, optimizer = optimizer, dataLoader = train_loader)
-print('one epoch trained created')
-print(train_loss)
-#pip install opencv-python==4.5.5.64
 
+train, test = get_data_loaders()
+
+
+for i, (image_train, mask_train) in enumerate(train):
+  break
+
+for i, (image_test, mask_test) in enumerate(test):
+  break
+
+
+print('train')
+print(f'image shape {image_train.shape}')
+print(f'mask shape {mask_train.shape}')
+
+print('test')
+print(f'image shape {image_test.shape}')
+print(f'mask shape {mask_test.shape}')
 '''
 
-for i,(image, mask) in enumerate(train_loader):
-    break
+model = models.detection.fasterrcnn_resnet50_fpn(pretrained = True, n_classes  = 2)
 
 
+#pytorch_total_params = sum(p.numel() for p in model.parameters())
 
 
-
-criterion = torch.nn.MSELoss(reduction='mean')
-# Specify the optimizer with a lower learning rate
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-model.train()
+for name, child in model.named_children():
+  print(f'name is: {name}')
+  print(f'module is: {child}')
 
 
+# freeze the backbone (it will freeze the body and fpn params)
+for p in model.backbone.parameters():
+  p.requires_grad = False
 
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-
-since = time.time()
-
-image = image.to(device)
-mask = mask.to(device)
-optimizer.zero_grad()
-out = model(image)
-loss = criterion(out['out'], mask)
-train_loss = loss.item()
-loss.backward()
-optimizer.step()
-
-till = time.time()
-
-
-
-def train_one_epoch(model, criterion, optimizer, dataLoader, device):
-    model.train()
-    total_loss = 0
-    for i, (image, mask) in enumerate(tqdm(dataLoader)):
-        image = image.to(device)
-        mask = mask.to(device)
-        optimizer.zero_grad()
-        out = model(image)
-        loss = criterion(out['out'], mask)
-        total_loss+=loss.item()
-        loss.backward()
-        optimizer.step()
-    return total_loss
-
-def val_one_epoch(model,criterion, dataLoader, device):
-    model.eval()
-    total_loss = 0
-    for i, (image, mask) in enumerate(tqdm(dataLoader)):
-        image = image.to(device)
-        mask = mask.to(device)
-        out = model(image)
-        loss = criterion(out['out'], mask)
-        total_loss+=loss.item()
-
-    return total_loss
-
-
-train_loss = train_one_epoch(model = model, criterion = criterion, optimizer=optimizer,
-                             device = device, dataLoader=train_loader)
-
-print(train_loss)
-
-
-
-
+# freeze the fc6 layer in roi_heads
+for p in model.roi_heads.box_head.fc6.parameters():
+  p.requires_grad = False
 '''
-
-
-
-
