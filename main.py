e67ece531e6c471f73ded25d7c00780acab39b6d
Andrii Zelenko <qvuer7@gmail.com>
2022-10-18 20:25:30 +0100

    detector trainers implemented under /utils/trainers.py get_faster_rcnn with frozen layers implemented under utils/models

--- main.py
+++ main.py
@@ -1,43 +1,14 @@
-from torchvision import models
-import xml.etree.ElementTree as et
-import os
-from config import *
-import cv2
-import albumentations as A
-from utils.vizualisation import *
-from torch.utils.data import Dataset
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from utils.data_loading import *
-from tqdm import tqdm
-
-
-
+from utils.models import *
+from utils.trainers import *
+from config import *
 
 
 train_loader, test_loader = get_detection_data_loaders()
+model = get_faster_rcnn()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0005)
+train_detector(model = model, trainLoader = train_loader, testLoader = test_loader, optimizer = optimizer)
 
 
 
-model = models.detection.fasterrcnn_resnet50_fpn(pretrained = True, n_classes  = 2)
 
-#pytorch_total_params = sum(p.numel() for p in model.parameters()
-# freeze the backbone (it will freeze the body and fpn params)
-for p in model.backbone.parameters():
-  p.requires_grad = False
-
-# freeze the fc6 layer in roi_heads
-for p in model.roi_heads.box_head.fc6.parameters():
-  p.requires_grad = False
-
-
-for i, data in enumerate(tqdm(train_loader)):
-    images, targets = data
-    images = list(image.to(DEVICE) for image in images)
-    targets = [{k: v.to(DEVICE) for k, v in t.items()} for t in targets]
-    break
-
-
-model.train()
-loss = model(images, targets)
-print(loss)
--- utils/models.py
+++ utils/models.py
@@ -7,3 +7,17 @@ def get_deep_lab_v3():
         parameter.requires_grad = False
     model.classifier = models.segmentation.deeplabv3.DeepLabHead(2048, N_CLASSES)
     return model
+
+
+def get_faster_rcnn():
+    model = models.detection.fasterrcnn_resnet50_fpn(pretrained = True, n_classes = N_CLASSES)
+    for p in model.backbone.parameters():
+        p.requires_grad = False
+
+    # freeze the fc6 layer in roi_heads
+    for p in model.roi_heads.box_head.fc6.parameters():
+        p.requires_grad = False
+
+    return model
+
+
