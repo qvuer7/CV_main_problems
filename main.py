06c12d81bac2c307449bb34f426d5a39ce84d2cb
Andrii Zelenko <qvuer7@gmail.com>
2022-10-21 18:37:34 +0100

    inference of mask_rcnn

--- main.py
+++ main.py
@@ -15,11 +15,32 @@ from torch.utils.data import Dataset
 import albumentations as A
 
 
+
+checkpoint_path = '/Users/andriizelenko/qvuer7/projects/CV_main_tasks/checkpoints/instance_segmentation/checkpoint_instance_segmentation.pth'
+model, _ = get_mask_rcnn()
+
+
+
+model.load_state_dict(torch.load(checkpoint_path, map_location=DEVICE))
+model.eval()
+
 train_df, test_df = get_instance_segmentation_dataframes()
-
 train_t, test_t = get_detection_transforms()
-md = InstanceSegmentationDataset(train_df, train_t)
+md = InstanceSegmentationDataset(test_df, transforms = test_t, labels_map = LABELS_MAP)
+i = 2
+image_m, target = md[i]
+image = image_m.clone().detach()
+image = image.unsqueeze(0)
+image = image.to(DEVICE)
 
-for i in range(len(md)):
-    image, data = md[i]
-    pass
+
+with torch.no_grad():
+    out = model(image)
+
+print(out)
+out = out[0]
+image = draw_bounding_box_from_ITtensor(image_s = image_m, target = out, label_map = LABELS_MAP)
+fig, (ax1, ax2) = plt.subplots(1,2)
+ax1.imshow(image)
+ax2.imshow(image)
+plt.show()
